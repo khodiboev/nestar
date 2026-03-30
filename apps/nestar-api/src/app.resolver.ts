b50e5b406274d8daf3087af3f18dce7b1781a3d4
Victor <khadibayevjurabek@gmail.com>
2026-03-31 01:46:13 +0900

    feat: integration GraphQL Module and build first GQL API

---
+++ apps/nestar-api/src/app.resolver.ts
@@ -0,0 +1,10 @@
+import { Res } from '@nestjs/common';
+import { Query, Resolver } from '@nestjs/graphql';
+
+@Resolver()
+export class AppResolver {
+	@Query(() => String)
+	public sayHello(): string {
+		return 'Hello from NestJS GraphQL API!';
+	}
+}
