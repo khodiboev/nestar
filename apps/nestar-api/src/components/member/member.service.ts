6d1b9d0bd5ef2363e8e0f2dc6189a56c880d6905
Victor <khadibayevjurabek@gmail.com>
2026-04-08 06:11:09 +0900

    feat: hashing password via bcrypt.js

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -5,25 +5,30 @@ import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class MemberService {
-	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+	constructor(
+		@InjectModel('Member') private readonly memberModel: Model<Member>,
+		private authService: AuthService,
+	) {}
 
 	public async signup(input: MemberInput): Promise<Member> {
-		// TODO: Hash password
+		input.memberPassword = await this.authService.hashPassword(input.memberPassword);
 		try {
 			const result = await this.memberModel.create(input);
 			// TODO: Authentication via TOKEN
 			return result;
 		} catch (err) {
-			console.log('Error, Service.model:', err.message);
+			// console.log('Error, Service.model:', err.message);
 			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
 		}
 	}
 
 	public async login(input: LoginInput): Promise<Member> {
 		const { memberNick, memberPassword } = input;
+		console.log('LoginInput:', input);
 		const response: Member | null = await this.memberModel
 			.findOne({ memberNick: memberNick })
 			.select('+memberPassword')
@@ -36,7 +41,7 @@ export class MemberService {
 		}
 
 		// TODO: Compare passwords
-		const isMatch = memberPassword === response.memberPassword;
+		const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
 
 		return response;
