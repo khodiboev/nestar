fc91ad0ea1d9fbc1ab6ddcd3f92917639bcde4e6
Victor <khadibayevjurabek@gmail.com>
2026-04-09 16:03:21 +0900

    feat: create checkAuthRoles & testing

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -6,6 +6,9 @@ import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import type { ObjectId } from 'mongoose';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 // Qabulxona — foydalanuvchidan kelgan so'rovlarni qabul qilib, Service ga uzatadi
 @Resolver()
@@ -34,21 +37,28 @@ export class MemberResolver {
 	// Authonticated
 	@UseGuards(AuthGuard)
 	@Mutation(() => String)
-	public async updateMember(@AuthMember("_id") memberId: ObjectId): Promise<String> {
+	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<String> {
 		console.log('Mutation: updateMember');
 		return this.memberService.updateMember();
 	}
 
-
-		@UseGuards(AuthGuard)
+	// Foydalanuvchi o'z ma'lumotlarini ko'radi (test)
+	@UseGuards(AuthGuard)
 	@Query(() => String)
-	public async checkAuth(@AuthMember("memberNick") memberNick: string): Promise<String> {
-		console.log('Mutation: checkAuth');
+	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<String> {
+		console.log('Query: checkAuth');
 		console.log('Authenticated memberNick:', memberNick);
 		return `Hi ${memberNick}`;
 	}
 
-
+	@Roles(MemberType.USER, MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Query(() => String)
+	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<String> {
+		console.log('Query: checkAuthRoles');
+		console.log('Authenticated memberNick:', authMember.memberNick);
+		return `Hi ${authMember.memberNick}, you are ${authMember.memberType}, (memberId: ${authMember._id})`;
+	}
 
 	// Bitta foydalanuvchi ma'lumotlarini qaytaradi (hali to'liq yozilmagan)
 	@Query(() => String)
@@ -59,10 +69,12 @@ export class MemberResolver {
 
 	/** ADMIN */
 
-	// Authorization: ADMIN
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
 	@Mutation(() => String)
-	public async getAllMembersByAdmin(): Promise<String> {
-		return '';
+	public async getAllMembersByAdmin(@AuthMember() authMember: Member): Promise<String> {
+		console.log('authMember.memberType:', authMember.memberType);
+		return this.memberService.updateAllMembersByAdmin();
 	}
 
 	// Authorization: ADMIN
