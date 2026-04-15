2b0927a748f2221e36fafa620b9d2244d63a3ed9
Victor <khadibayevjurabek@gmail.com>
2026-04-15 20:39:33 +0900

    feat: modify upload images configurations

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
+import { graphqlUploadExpress } from "graphql-upload";
+import * as express from "express";
 
 // Dasturni yoqadigan asosiy funksiya
 async function bootstrap() {
@@ -13,6 +15,11 @@ async function bootstrap() {
 
 	// Har bir so'rov kelganda va javob ketganda uni logga yozsin
 	app.useGlobalInterceptors(new LoggingInterceptor());
+
+	app.enableCors({origin: true, credentials: true});
+
+	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
+	app.use("/uploads", express.static("./uploads"));
 
 	// Server "eshikni ochadi" — PORT_API .env dan olinadi, aks holda 3000-port ishlatiladi
 	await app.listen(process.env.PORT_API ?? 3000);
