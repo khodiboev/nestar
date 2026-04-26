e15a8e51aab2f26952b55f1872f148c6ca6db523
Victor <khadibayevjurabek@gmail.com>
2026-04-26 23:31:59 +0900

    feat: modify job scheduler business logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
-import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { Cron, Timeout } from '@nestjs/schedule';
 import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
@@ -10,10 +10,10 @@ export class BatchController {
 
 	@Timeout(1000)
 	handleTimeout() {
-		this.logger.debug('BATCH SEVER READY!');
+		this.logger.debug('BATCH SERVER READY!');
 	}
 
-	@Cron('00 * * * * *', { name: BATCH_ROLLBACK })
+	@Cron('00 00 01 * * *', { name: BATCH_ROLLBACK })
 	public async batchRollback() {
 		try {
 			this.logger['context'] = BATCH_ROLLBACK;
@@ -24,37 +24,25 @@ export class BatchController {
 		}
 	}
 
-	@Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
-	public async batchProperties() {
+	@Cron('20 00 01 * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchTopProperties() {
 		try {
 			this.logger['context'] = BATCH_TOP_PROPERTIES;
 			this.logger.debug('EXECUTED!');
-			await this.batchService.batchProperties();
+			await this.batchService.batchTopProperties();
 		} catch (err) {
 			this.logger.error(err);
 		}
 	}
 
-	@Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
-	public async cronTest() {
+	@Cron('40 00 01 * * *', { name: BATCH_TOP_AGENTS })
+	public async batchTopAgents() {
 		try {
 			this.logger['context'] = BATCH_TOP_AGENTS;
 			this.logger.debug('EXECUTED!');
-			await this.batchService.batchAgents();
+			await this.batchService.batchTopAgents();
 		} catch (err) {
 			this.logger.error(err);
 		}
-	}
-
-	/*
-  @Interval(1000)
-  handleInterval() {
-    this.logger.debug('Called every second');
-  }
-  */
-
-	@Get()
-	getHello(): string {
-		return this.batchService.getHello();
 	}
 }
--- apps/nestar-batch/src/batch.module.ts
+++ apps/nestar-batch/src/batch.module.ts
@@ -4,10 +4,19 @@ import { BatchService } from './batch.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import PropertySchema from 'apps/nestar-api/src/schemas/Property.model';
+import MemberSchema from 'apps/nestar-api/src/schemas/Member.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
-	controllers: [BatchController],
-	providers: [BatchService],
+    imports: [
+        ConfigModule.forRoot(),
+        DatabaseModule,
+        ScheduleModule.forRoot(),
+        MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]),
+        MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
+    ],
+    controllers: [BatchController],
+    providers: [BatchService],
 })
 export class BatchModule {}
